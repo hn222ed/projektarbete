7ba8f6fb7fb22a476b9bacf89e88b408c5423e35
Ellen Bihl <eb222sp@student.lnu.se>
2014-01-17 09:50:30 -0500

    push nr 2 17 jan

--- form.js
+++ form.js
@@ -1,18 +1,17 @@
 document.getElementById("submit").onclick = function (e) {
     var name = document.getElementById("namn").value;
     var  email = document.getElementById("mail").value;
+    var svar = document.getElementById('svar');
+
     
     
     if(name.length < 1) {
-       
-        alert("Du har glömt fylla i ditt namn");
-        
+        svar.innerHTML = "<p>Please fill in your name</p>"+ svar.innerHTML;
+
         
         e.preventDefault();
     }
     if(email.length < 1) {
-        alert("Du har glömt fylla i din epost");
-        e.preventDefault(); 
-    }
-    
+        svar.innerHTML = "<p>Please type your e-mail</p>"+ svar.innerHTML;  
 }
+
